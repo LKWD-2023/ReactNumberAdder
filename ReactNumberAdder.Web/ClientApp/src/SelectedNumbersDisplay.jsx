f7f13d6e378590ece566e0e7e218b42da766625c
Avrumi Friedman <info@lakewoodprogramming.com>
2023-05-07 23:52:07 -0400

    Add project files.

---
+++ ReactNumberAdder.Web/ClientApp/src/SelectedNumbersDisplay.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class SelectedNumbersDisplay extends React.Component {
+    render() {
+        const { numbers, onLockChangeClick, lockedNumbers } = this.props;
+        return (
+            <div className="row p-5 rounded" style={{backgroundColor: '#E9ECEF'}}>
+                <div className="col-md-6 col-md-offset-3">
+                    <h3>Selected Numbers </h3>
+                    <ul className="list-group">
+                        {numbers.map(n => <li className="list-group-item" key={n.id}>
+                            {n.number}
+                            <button className="ms-5 btn btn-primary"
+                                onClick={() => onLockChangeClick(n.id)}>{lockedNumbers.includes(n.id) ? "Unlock" : "Lock"}</button>
+                        </li>)}
+                    </ul>
+                </div>
+            </div>)
+    }
+}
+
+export default SelectedNumbersDisplay;
